86eae0babddead063c64b0b39bad059055a00fea
Sea Grapes <natuloid@gmail.com>
2025-08-05 21:19:30 -0700

    bug fix: local glob

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,8 +1,9 @@
-import { parse, type PreprocessorGroup } from 'svelte/compiler'
-import { unified } from 'unified'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import { parse } from 'svelte/compiler'
+import { globSync } from 'tinyglobby'
+import { unified } from 'unified'
 
 async function parse_md(string: string) {
   let res = await unified()
@@ -16,7 +17,7 @@ async function parse_md(string: string) {
   return res.toString()
 }
 
-async function html(content: string) {
+async function parse_svm(content: string) {
   const svast = parse(content)
   const { start, end } = svast.html
   const string = content.slice(start, end)
@@ -30,17 +31,25 @@ async function html(content: string) {
 }
 
 function markdown() {
+  const layout_data = {}
+  const layout_paths = globSync('./**/layout.md.svelte')
+  console.log(layout_paths)
+
+  // const watcher = chokidar.watch('.', {
+  //   ignored: (path: string, stats?: Stats) =>
+  //     (stats?.isFile() && !path.endsWith('.svelte')) || false,
+  //   persistent: true,
+  // })
+
+  // watcher.on('change', (path: string) => {})
+
   return {
     name: 'markdown',
     markup({ content, filename }: { content: string; filename: string }) {
-      console.log(filename)
+      // console.log(filename)
 
       if (filename.endsWith('.md')) {
-        return html(content)
-      }
-
-      if(filename.endsWith('layout.md.svelte')) {
-        
+        return parse_svm(content)
       }
     },
   }
