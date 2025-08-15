893e1def367cab058f74ab9ca27ac0e6684890de
Sea Grapes <natuloid@gmail.com>
2025-08-15 10:52:11 -0700

    up

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -8,8 +8,6 @@ import { globSync } from 'tinyglobby'
 import { unified } from 'unified'
 import { parseEntities } from 'parse-entities'
 
-import { visit } from 'unist-util-visit'
-
 type Extension = '.md' | '.svelte' | '.svx' | (string & {})
 
 interface PluginConfig {
@@ -47,55 +45,7 @@ function get_layout_paths(filename: string): string[] {
     .map((str) => '/' + str)
 }
 
-/*
-function rawHtml() {
-  return (tree: Root) => {
-    visit(tree, 'html', (node) => {
-      console.log(node.type)
-      // Turn the HTML into a "raw" HTML node so it's passed through
-      // @ts-ignore
-      node.type = 'raw'
-    })
-  }
-}
-
-function splitSvelteBlocks() {
-  return (tree: Root) => {
-    visit(tree, 'paragraph', (node, index, parent) => {
-      if (!parent || index === undefined) return
-
-      // Check if paragraph has Svelte blocks
-      const hasSvelteBlocks = node.children.some(
-        (child) => child.type === 'text' && /\{[^}]+\}/.test(child.value)
-      )
-
-      if (!hasSvelteBlocks) return
-
-      // For now, just convert the whole paragraph to HTML
-      // (We can get fancier later if needed)
-      parent.children[index] = {
-        type: 'html',
-        value: node.children.map((child) => child?.value || '').join(''),
-      }
-    })
-  }
-}*/
-
-// function svelteLogic() {
-//   return(tree: Root) => {
-//     visit(tree, 'paragraph', (node, index, parent) => {
-//       if(!parent || !index) return;
-
-//     })
-//   }
-// }
-
-// function rehypeSvelteLogic() {
-//   return(tree: Root) => {
-
-//   }
-// }
-
+// allowDangerousHtml = allow script tag
 const md_parser = unified()
   .use(remarkParse)
   .use(remarkRehype, {
@@ -129,29 +79,18 @@ async function parse_svm(md_file: string, filename: string) {
   console.log('starting file')
   console.log(content)
 
-  // res = res.replace(/\{[#/:@][^}]*\}/g, (match) => {
-  //   const id = `\n\n%%SVELTEMD_${save.length}%%\n\n`
-  //   save.push(match)
-  //   return id
-  // })
-
   res = res.replace(/\{[#/:@][^}]*\}/g, (match) => {
     const id = `<div data-svelte-block="${save.length}"></div>`
     save.push(match)
     return id
   })
 
-  res = parseEntities(md_to_html_str(res))
+  res = md_to_html_str(res)
+  res = parseEntities(res)
 
   save.forEach((text, i) => {
     res = res.replace(`<div data-svelte-block="${i}"></div>`, text)
   })
-
-  // res = res.replace(/<p>\s*(%%SVELTEMD_\d+%%)\s*<\/p>/g, '$1')
-
-  // save.forEach((text, i) => {
-  //   res = res.replace(`%%SVELTEMD_${i}%%`, text)
-  // })
 
   console.log('final output:')
   console.log(res)
--- test-app/src/routes/+page.md
+++ test-app/src/routes/+page.md
@@ -1,18 +1,15 @@
 # Welcome to cool site
 
 This page is rendered using markdown inside a svelte component.
-
 Check out the [blog](blog) for some cool examples!
 
-Here is how this page works:
+Here is the code for this page:
 
 ```md
-<section>
-
 # Welcome to cool site
 
 This page is rendered using markdown inside a svelte component.
 Check out the [blog](blog) for some cool examples!
 
-</section>
+Here is the code for this page:
 ```
