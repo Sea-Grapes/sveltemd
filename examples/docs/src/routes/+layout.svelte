02ff9cbce4c746f0643e5b929f3c01621e0786d8
Sea Grapes <natuloid@gmail.com>
2025-08-18 11:48:57 -0700

    docs

--- examples/docs/src/routes/+layout.svelte
+++ examples/docs/src/routes/+layout.svelte
@@ -9,13 +9,28 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav
-  class="py-4 px-4 sm:px-6 xl:px-8 2xl:px-40 flex justify-between font-medium items-center"
+<main class="min-h-screen">
+  <nav
+    class="py-4 px-4 sm:px-6 xl:px-8 2xl:px-40 flex justify-between font-medium items-center"
+  >
+    <a href="/" class="text-lg">Sveltemd</a>
+    <div class="flex gap-8 text-[15px] text-zinc-600">
+      <a href="/docs">Docs</a>
+      <a href="/docs">Reference</a>
+    </div>
+  </nav>
+  {@render children?.()}
+</main>
+
+<footer
+  class="border-t border-t-zinc-300 px-4 pt-8 pb-9 text-center font-medium text-zinc-500 text-sm leading-relaxed"
 >
-  <a href="/" class="text-lg">Sveltemd</a>
-  <div class="flex gap-8 text-[15px] text-zinc-600">
-    <a href="/docs">Docs</a>
-    <a href="/docs">Reference</a>
-  </div>
-</nav>
-{@render children?.()}
+  <p>Released under the MIT License.</p>
+  <p>
+    Design copied from <a
+      href="https://vitepress.dev/"
+      target="_blank"
+      class="text-zinc-600 underline">VitePress</a
+    >
+  </p>
+</footer>
