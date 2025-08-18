568d31b7262c4eefa3d6d4ab9b98db19920a72b9
Sea Grapes <natuloid@gmail.com>
2025-08-18 11:42:52 -0700

    docs

--- examples/docs/src/routes/+layout.svelte
+++ examples/docs/src/routes/+layout.svelte
@@ -9,7 +9,13 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class="py-4 px-1-5 flex justify-between">
-  <a href="/" class="font-medium">Sveltemd</a>
+<nav
+  class="py-4 px-4 sm:px-6 xl:px-8 2xl:px-40 flex justify-between font-medium items-center"
+>
+  <a href="/" class="text-lg">Sveltemd</a>
+  <div class="flex gap-8 text-[15px] text-zinc-600">
+    <a href="/docs">Docs</a>
+    <a href="/docs">Reference</a>
+  </div>
 </nav>
 {@render children?.()}
