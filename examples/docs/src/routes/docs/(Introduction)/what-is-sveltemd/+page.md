14651af9862c1930972573e5c4714e025c8c9bd8
Sea Grapes <natuloid@gmail.com>
2025-08-22 19:20:21 -0700

    update docs

--- examples/docs/src/routes/docs/(Introduction)/what-is-sveltemd/+page.md
+++ examples/docs/src/routes/docs/(Introduction)/what-is-sveltemd/+page.md
@@ -5,4 +5,18 @@ index: 0
 
 Sveltemd is primarily a Markdown Preprocessor for [Svelte](https://svelte.dev/) that allows you to seamlessly mix markdown and svelte content. However, it also aims to be a comprehensive solution for authoring content in your projects. This can include anything from blog posts to full-blown documentation sites, with limitless customization out of the box.
 
-Here is an example:
+You can mix markdown and svelte very tightly. Here is an example:
+
+```svelte
+<script>
+  import Comp from './Comp.svelte'
+  let count = $state(0)
+</script>
+
+# Example page
+
+This is a test sveltemd page.
+
+<button>**Clicked** {count} _times_</button>
+<Comp />
+```
--- examples/docs/svelte.config.js
+++ examples/docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { markdown } from 'sveltemd'
 import { titleCase } from 'title-case'
@@ -13,24 +13,24 @@ const config = {
     markdown({
       code: {
         shiki_options: {
-          theme: 'vitesse-light',
-        },
+          theme: 'vitesse-light'
+        }
       },
       frontmatter: (data) => {
         console.log('PROCESS')
         console.log(data)
         if (data.title) data.title = titleCase(data.title)
         return data
-      },
-    }),
+      }
+    })
   ],
 
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
-  },
+    adapter: adapter()
+  }
 }
 
 export default config
