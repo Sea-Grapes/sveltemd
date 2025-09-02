51552b015c5d0eb97f0462e4578850f1f45a2b37
Sea Grapes <natuloid@gmail.com>
2025-09-02 12:24:02 -0700

    test

--- src/unified/micromark.ts
+++ src/unified/micromark.ts
@@ -22,6 +22,11 @@ export function svmdExtension(): Extension {
   }
 }
 
+// This is a somewhat ported version of the svelte bracket match util
+// Also see micromark-extension-mdx-jsx
+// https://github.com/sveltejs/svelte/blob/main/packages/svelte/src/compiler/phases/1-parse/utils/bracket.js
+// https://github.com/micromark/micromark-extension-mdx-jsx/blob/main/dev/lib/factory-tag.js
+
 const handleSvelteBlock: Tokenizer = function (effects, ok, nok) {
   console.log('inside handler')
   let depth = 0
@@ -51,13 +56,132 @@ const handleSvelteBlock: Tokenizer = function (effects, ok, nok) {
 
   function inside(code: Code): State {
     if (code === null) return nok(code) as State
-    if (code === '}'.charCodeAt(0)) {
+
+    if (code === '{'.charCodeAt(0)) {
+      depth++
       effects.consume(code)
-      effects.exit('svelteBlock')
-      return ok
+      return inside
     }
+
+    if (code === '}'.charCodeAt(0)) {
+      depth--
+      effects.consume(code)
+      if (depth == 0) {
+        effects.exit('svelteBlock')
+        return ok
+      }
+    }
+
+    if (
+      code === `"`.charCodeAt(0) ||
+      code === `'`.charCodeAt(0) ||
+      code === '`'.charCodeAt(0)
+    ) {
+      effects.consume(code)
+      return inString(code)
+    }
+
+    if (code === '/'.charCodeAt(0)) {
+      return afterSlash
+    }
+
     effects.consume(code)
     return inside
+  }
+
+  function afterSlash(code: Code): State {
+    if (code === '/'.charCodeAt(0)) {
+      effects.consume(code)
+      return inLineComment
+    }
+
+    if (code === '*'.charCodeAt(0)) {
+      effects.consume(code)
+      return inBlockContent
+    }
+
+    return inRegex(code)
+  }
+
+  function inLineComment(code: Code): State {
+    if (code === null) return nok(code) as State
+    effects.consume(code)
+    // Todo: eval correctness
+    if (code === '\n'.charCodeAt(0)) return inside
+    return inLineComment
+  }
+
+  function inBlockContent(code: Code): State {
+    if (code === null) return nok(code) as State
+    effects.consume(code)
+    if (code === '*'.charCodeAt(0)) return afterBlockCommentStar
+    return inBlockContent
+  }
+
+  function afterBlockCommentStar(code: Code): State {
+    // only break out of comment if exactly "*/"
+    if (code === '/'.charCodeAt(0)) {
+      effects.consume(code)
+      return inside
+    }
+    return inBlockContent(code)
+  }
+
+  // Todo: evaluate correctness
+  function inRegex(code: Code): State {
+    if (code === null) return nok(code) as State
+    effects.consume(code)
+
+    if (code === '/'.charCodeAt(0)) return inside
+
+    // singular slash character, just js strings escape it
+    if (code === '\\'.charCodeAt(0)) {
+      return escapeCode(inRegex)
+    }
+
+    return inRegex
+  }
+
+  function inString(quote: Code): State {
+    // a second function required so we can store the quote character^ above,
+    // and thus match the correct one
+    return function currentString(code: Code): State {
+      if (code === null) return nok(code) as State
+      effects.consume(code)
+
+      if (code === quote) return inside
+
+      if (code === '\\'.charCodeAt(0)) {
+        return escapeCode(currentString)
+      }
+
+      if (quote === '`'.charCodeAt(0) && code === '$'.charCodeAt(0)) {
+        return inStringTemplate
+      }
+
+      return currentString
+    }
+  }
+
+  function inStringTemplate(code: Code): State {
+    if (code === '{'.charCodeAt(0)) {
+      effects.consume(code)
+      depth++
+      return inside
+    }
+
+    // Todo: evaluate correctness (possible undef?)
+    // continue with same string type
+    return inString('`'.charCodeAt(0))(code) as State
+  }
+
+  // consumes escape character \ plus its next character
+  function escapeCode(next: (code: Code) => State): State {
+    return function escaped(code: Code): State {
+      if (code === null) return nok(code) as State
+      effects.consume(code)
+      return next
+    }
   }
 }
 
